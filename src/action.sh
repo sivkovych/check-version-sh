dd9cfc6258d65f0d6e4c5e9ad6fece8030c740a5
sivkovych <sivkovych.p@gmail.com>
2023-09-28 19:49:12 +0300

    - **1.3.2**     - Fix failed files logging     - Add missing files log message

--- src/action.sh
+++ src/action.sh
@@ -44,18 +44,25 @@ for version_file in "${PROJECT_DIR}"/version-in/*.sh; do
     check_version::apply "${version_file}" "${diff_ref}"
     check_result="${?}"
     check_results+=("${check_result}")
-    failed_labels+=("${check_label}")
+    if [ ${check_result} -eq 1 ]; then
+        failed_labels+=("${check_label}")
+    elif [ ${check_result} -eq 66 ]; then
+        missing_labels+=("${check_label}")
+    fi
     log::debug "Received exit code from version check [${check_result}] for [${check_label}]"
 done
 log::debug "Version Checks exit codes: [${check_results[*]}]"
 if array::contains 1 "${check_results[@]}"; then
     log::error "Failed labels: [${failed_labels[*]}]"
+    if array::contains 66 "${check_results[@]}"; then
+        log::error "Missing changes in [${missing_labels[*]}]"
+    fi
     log::fail "Version Check failed"
 fi
 if array::every 66 "${check_results[@]}"; then
     log::fail "No changed/supported version files found"
 fi
-if ! array::every 0 "${check_results[@]}" && [ -n "${check_only_for}" ]; then
+if ! array::every 0 "${check_results[@]}" && [ -n "${check_only_for[*]}" ]; then
     log::error "Failed labels: [${failed_labels[*]}]"
     log::fail "Not all specified file checks were successful"
 fi
